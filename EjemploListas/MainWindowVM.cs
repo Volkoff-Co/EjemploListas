644465a5113cef9985890c6ef3ba1ea35b1217bc
alumno <alumno@A302PC14>
2022-11-09 12:55:18 +0100

    Agregar archivos de proyecto.

---
+++ EjemploListas/MainWindowVM.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace EjemploListas
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        private Persona personaActual;
+
+        private ObservableCollection<Persona> personas;
+
+        public MainWindowVM()
+        {
+            personas = new ObservableCollection<Persona>();
+            personas.Add(new Persona("Gregory", 22));
+            personas.Add(new Persona("Alejandro", 21));
+            personas.Add(new Persona("Salvador", 23));
+        }
+        public ObservableCollection<Persona> Personas
+        {
+            get { return personas; }
+            set
+            {
+                personas = value;
+                NotifyPropertyChanged("Personas");
+            }
+        }
+
+        public Persona PersonaActual
+        {
+            get { return personaActual; }
+            set {
+                personaActual = value;
+                NotifyPropertyChanged("PersonaActual");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ EjemploListas/Persona.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploListas
+{
+    class Persona: INotifyPropertyChanged
+    {
+        private string _nombre;
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set
+            {
+                if (this._nombre != value)
+                {
+                    this._nombre = value;
+                    this.NotifyPropertyChanged("Nombre");
+                }
+            }
+        }
+
+        private int _edad;
+        public int Edad
+        {
+            get { return this._edad; }
+            set
+            {
+                if (this._edad != value)
+                {
+                    this._edad = value;
+                    this.NotifyPropertyChanged("Edad");
+                }
+            }
+        }
+        public Persona()
+        {
+            Nombre = "";
+            Edad = 0;
+        }
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
